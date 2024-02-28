07a7e63b06807cdd61629d21e09edcd9e4735016
WalterHuarcaya <eduardohuarcaya04@gmail.com>
2024-02-28 17:21:06 -0500

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,14 +11,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://WalterHuarcaya.github.io',
+  url: 'https://prueba1.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/walterLC.github.io/',
+  baseUrl: '/prueba1/',
 
   // GitHub pages deployment config.
   organizationName: 'WalterHuarcaya',
-  projectName: 'walterLC.github.io',
+  projectName: 'prueba1',
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
